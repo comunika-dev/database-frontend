e68f27b97ba05fbeb48ae1cc597b73922a241de2
rutonc <rutoncode@gmail.com>
2024-09-24 09:43:42 +0200

    feat:add new url base

--- src/utils/constants-base.ts
+++ src/utils/constants-base.ts
@@ -1,4 +1,5 @@
 export const constants = {
   token:'d81ae24f6c9ff2b35b319f921419f739',
-  baseURL:'http://localhost:3333'
+  // baseURL:'http://localhost:3333'
+  baseURL:'https://api.comunika.co.mz'
 }
